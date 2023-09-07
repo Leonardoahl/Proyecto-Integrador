03f05547b4217b33275d2aacfba7d583b0aa6095
Jimena Barranco Medina <Jime@192.168.1.12>
2023-09-06 22:24:42 -0600

    fix:name valitation with bootstrap

--- assets/js/register.js
+++ assets/js/register.js
@@ -23,13 +23,15 @@ registerForm.addEventListener("submit", (event)=> {event.preventDefault()
 
 function validateRegistration(userData) {
 
-    if (validateName(userData)&& validateLastName(userData) && validatePassword(userData)&& confirmPassword(userData) && validateEmail(userData)) {
+    if (validateName(userData)&& validateLastName(userData) && 
+        validatePassword(userData)&& 
+        confirmPassword(userData) && 
+        validateEmail(userData)) {
         // Hacer lo del JJJJJJSSSSSOOOOOONNNNNNNNNNNN
         console.log("Registro exitoso");
+        //registerForm.classList.add('was-validated');
         registerForm.reset();
-    } else {
-        printAlerts(userData);
-    }
+    } 
 }
 
 function validateName ({name}){
@@ -38,10 +40,18 @@ function validateName ({name}){
       const patronMatch = name.match(patron);
       let isValid = false;
       
-      if(patronMatch){isValid = true;
-      return isValid;
-  }
-      else { console.log("No validó")};
+      if (patronMatch) {
+        isValid = true;
+        // Campo válido, así que eliminamos la clase 'is-invalid' si está presente.
+        const nameInput = document.getElementById("inputFirstName");
+        nameInput.classList.remove("is-invalid");
+        return isValid;
+      } else {
+        console.log("No validó");
+        // Campo no válido, agregamos la clase 'is-invalid'.
+        const nameInput = document.getElementById("inputFirstName");
+        nameInput.classList.add("is-invalid");
+      };
   
   };
   function validateLastName ({lastName}){
@@ -60,7 +70,7 @@ function validateName ({name}){
 function validatePassword({password}){
     console.log("entrando a validar pass");
     let isValid = false;
-    if (password.length >=8){ isValid = true;
+    if ((password.length >=8)){ isValid = true;
         return isValid;
     }
      
@@ -80,79 +90,7 @@ function validateEmail({email}){
     const emailMatch = email.match(emailPattern);
     return emailMatch !== null;
 }
-function printAlerts(userData) {
-    //Variables para nombre
-    const nameInput = document.getElementById("inputFirstName");
-    const nameError = document.getElementById("nameError");
-    const nameSuccess = document.getElementById("nameSuccess");
-    //Variables para apellido
-    const lastNameInput = document.getElementById("inputLastName");
-    const lastNameError = document.getElementById("lastNameError");
-    const lastNameSuccess = document.getElementById("lastNameSuccess");
-    //Variables para password
-    const passwordInput = document.getElementById("password");
-    const passwordError = document.getElementById("passwordError");
-    const passwordSuccess = document.getElementById("passwordSuccess");
-    //Variables para confirmar password
-    const confirmedPassword = document.getElementById("confirmedPassword");
-    const confirmedPasswordError = document.getElementById("confirmedPasswordError");
-    const confirmedPasswordSuccess = document.getElementById("confirmedPasswordSuccess");
-    //Variables para email
-    const emailInput = document.getElementById("email");
-    const emailError = document.getElementById("emailError");
-    const emailSuccess = document.getElementById("emailSuccess");
 
-    //habilitar-deshabilitar validaciones
-    
-    if (!validateName(userData)) {
-      nameInput.classList.add("is-invalid");
-      nameInput.classList.remove("is-valid");
-      nameError.style.display = "block"; // Muestra el mensaje de error
-      nameSuccess.style.display = "none"; // Oculta el mensaje de éxito
-    } else {
-      nameInput.classList.remove("is-invalid");
-      nameInput.classList.add("is-valid");
-      nameError.style.display = "none"; // Oculta el mensaje de error
-      nameSuccess.style.display = "block"; // Muestra el mensaje de éxito
-    }
-    
-    if (!validateLastName(userData)) {
-      lastNameInput.classList.add("is-invalid");
-      lastNameError.style.display = "block";
-      lastNameSuccess.style.display = "none"; 
-    } else {
-      lastNameInput.classList.remove("is-invalid");
-      lastNameError.style.display = "none"; 
-      lastNameSuccess.style.display = "block"; 
-    }
-  
-    if (!validatePassword(userData)) {
-      passwordInput.classList.add("is-invalid");
-      passwordError.style.display = "block";
-      passwordSuccess.style.display = "none";
-    } else {
-      passwordInput.classList.remove("is-invalid");
-      passwordError.style.display = "none"; 
-      passwordSuccess.style.display = "block"; 
-    }
-    if (!confirmPassword(userData)) {
-      confirmedPassword.classList.add("is-invalid");
-      confirmedPasswordError.style.display = "block";
-      confirmedPasswordSuccess.style.display = "none";
-      } else {
-      confirmedPassword.classList.remove("is-invalid");
-      confirmedPasswordError.style.display = "none"; 
-      confirmedPasswordSuccess.style.display = "block";
-      }
-  
-    if (!validateEmail(userData)) {
-      emailInput.classList.add("is-invalid");
-      emailError.style.display = "block";
-      emailSuccess.style.display = "none";
-    } else {
-      emailInput.classList.remove("is-invalid");
-      emailError.style.display = "none"; 
-      emailSuccess.style.display = "block";
-    }
-  }
-  
+
+
+

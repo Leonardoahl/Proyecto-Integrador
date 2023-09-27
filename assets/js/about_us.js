384b046f5306be31e12169e3179f69ded601a44b
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-09-27 09:52:30 -0600

    ci: update carousel

--- assets/js/about_us.js
+++ assets/js/about_us.js
@@ -3,3 +3,15 @@
  * @version 0.0.1
  * 
  */
+$(document).ready(function() {
+    $('.img-container img').click(function() {
+      // Oculta todas las listas al principio
+      $('.hidden-list').hide();
+  
+      // Muestra la lista correspondiente a la imagen clicada
+      var listaId = $(this).closest('.carousel-item').find('h5').text();
+      listaId = listaId.replace(/\s+/g, '-').toLowerCase(); // Convierte el nombre en un identificador válido
+      $('#' + listaId).show();
+    });
+  });
+  

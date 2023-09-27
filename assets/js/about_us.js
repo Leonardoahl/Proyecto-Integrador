c6a9bb6962b56bd8080f472e280fa2a7efede457
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-09-27 16:37:27 -0600

    ci:final carosuel

--- assets/js/about_us.js
+++ assets/js/about_us.js
@@ -2,23 +2,28 @@
  * @author Perry Code
  * @version 0.0.1
  * 
- */
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtiene todos los elementos de imagen dentro de .img-container
-    var imgElements = document.querySelectorAll('.img-container img');
-  
-    imgElements.forEach(function(img) {
-      img.addEventListener('click', function() {
-        // Oculta todas las listas al principio
-        var hiddenLists = document.querySelectorAll('.hidden-list');
-        hiddenLists.forEach(function(list) {
-          list.style.display = 'none';
-        });
-  
-        // Muestra la lista correspondiente a la imagen clicada
-        var listaId = this.closest('.carousel-item').querySelector('h5').textContent;
-        listaId = listaId.replace(/\s+/g, '-').toLowerCase();
-        document.getElementById(listaId).style.display = 'block';
-      });
+*/
+const imagenesCarrusel = document.querySelectorAll('.open-modal');
+const descripciones = document.querySelectorAll('.image-description');
+
+// Agrega un evento click a cada imagen
+imagenesCarrusel.forEach((imagen, index) => {
+  imagen.addEventListener('click', () => {
+    // Oculta todas las descripciones
+    descripciones.forEach((descripcion) => {
+      descripcion.style.display = 'none';
     });
+
+    // Muestra la descripción correspondiente
+    descripciones[index].style.display = 'block';
   });
+});
+
+// Agrega un evento click al cuerpo de la página para ocultar la descripción
+document.body.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('open-modal')) {
+    descripciones.forEach((descripcion) => {
+      descripcion.style.display = 'none';
+    });
+  }
+});

6a1fbcf9cc3add2539b1824b7543091684bb12c0
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-09-27 21:33:02 -0600

    fix:about_us.js

--- assets/js/about_us.js
+++ assets/js/about_us.js
@@ -3,30 +3,7 @@
  * @version 0.0.1
  * 
 */
-/* const imagenesCarrusel = document.querySelectorAll('.open-modal');
-const descripciones = document.querySelectorAll('.image-description');
 
-// Agrega un evento click a cada imagen
-imagenesCarrusel.forEach((imagen, index) => {
-  imagen.addEventListener('click', () => {
-    // Oculta todas las descripciones
-    descripciones.forEach((descripcion) => {
-      descripcion.style.display = 'none';
-    });
-
-    // Muestra la descripción correspondiente
-    descripciones[index].style.display = 'block';
-  });
-});
-
-// Agrega un evento click al cuerpo de la página para ocultar la descripción
-document.body.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('open-modal')) {
-    descripciones.forEach((descripcion) => {
-      descripcion.style.display = 'none';
-    });
-  }
-}); */
 // Obtén una lista de todas las imágenes con la clase "open-modal"
 var images = document.querySelectorAll('.open-modal');
 

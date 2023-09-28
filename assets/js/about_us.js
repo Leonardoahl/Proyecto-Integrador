72451d053b52632d3513997b0b0137bbc31b0983
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-09-27 21:35:49 -0600

    fix:aboutus.js

--- assets/js/about_us.js
+++ assets/js/about_us.js
@@ -4,16 +4,15 @@
  * 
 */
 
-
 var images = document.querySelectorAll('.open-modal');
 
-// Agrega un evento de clic a cada imagen
+
 images.forEach(function (image) {
   image.addEventListener('click', function () {
-    // Obtén el elemento de descripción de la imagen clicada
+  
     var description = this.closest('.carousel-item').querySelector('.image-description');
 
-    // Alterna la visibilidad del elemento de descripción
+
     if (description.style.display === 'none' || description.style.display === '') {
       description.style.display = 'block';
     } else {
